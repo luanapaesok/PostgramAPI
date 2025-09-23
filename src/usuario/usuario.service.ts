08bc285e218b90edd945c709ef4f33e85f471781
luanapaesok <luanapaes.ok@gmail.com>
2025-09-23 12:46:17 -0300

    comenta tentativa de validação

--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -8,24 +8,32 @@ import { UpdateUsuarioDTO } from "./dto/update-usuario.dto";
 @Injectable()
 export class UsuarioService {
     constructor(
-        @InjectRepository(Usuario) private usuarioEntity: Repository<Usuario>
+        @InjectRepository(Usuario) private usuarioRepository: Repository<Usuario>
     ) { }
 
     async verificarSeExistePeloID(id: number) {
-        if (!(await this.usuarioEntity.exists({
+        if (!(await this.usuarioRepository.exists({
             where: { id }
         }))) {
             throw new BadRequestException(`Usuário com id ${id} não encontrado.`)
         }
     }
 
-    create(data: CreateUsuarioDTO) {
-        let user = this.usuarioEntity.create(data);
-        return this.usuarioEntity.save(user)
+    async create(data: CreateUsuarioDTO) {
+        if (await this.usuarioRepository.exists({
+            where: {
+                email: data.email
+            }
+        })) {
+            throw new BadRequestException("Este e-mail já está cadastrado.");
+        }
+
+        let user = this.usuarioRepository.create(data);
+        return this.usuarioRepository.save(user)
     }
 
     getAll() {
-        return this.usuarioEntity.find({
+        return this.usuarioRepository.find({
             relations: ["posts"]
         });
     }
@@ -33,17 +41,31 @@ export class UsuarioService {
     async getByID(id: number) {
         await this.verificarSeExistePeloID(id);
 
-        return await this.usuarioEntity.findOne({
+        return await this.usuarioRepository.findOne({
             where: { id },
             relations: ["posts"]
         });
     }
 
     async delete(id: number) {
-        return this.usuarioEntity.delete(id)
+        return this.usuarioRepository.delete(id)
     }
 
-    update(updateUserDto: UpdateUsuarioDTO) {
-        return this.usuarioEntity.update(updateUserDto.id, updateUserDto);
+    async update(updateUserDto: UpdateUsuarioDTO) {
+        return this.usuarioRepository.update(updateUserDto.id, updateUserDto);
+
+        // const user = await this.getByID(updateUserDto.id)
+
+        // const teste = await this.usuarioRepository.exists({
+        //     where: {
+        //         email: updateUserDto.email
+        //     }
+        // })
+        // console.log(teste)
+        // if(!teste){
+        //     return this.usuarioRepository.update(updateUserDto.id, updateUserDto);
+        // } else {
+        //     throw new BadRequestException("Este e-mail já existe.")
+        // }
     }
 }
