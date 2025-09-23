3449a232eba42ad7faf492c36fa3d98238257561
luanapaesok <luanapaes.ok@gmail.com>
2025-09-23 08:25:16 -0300

    feat: add App

---
+++ src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Usuario } from './usuario/entity/usuario.entity';
+import { ConfigModule } from '@nestjs/config';
+import { UsuarioModule } from './usuario/usuario.module';
+import { Post } from './post/entity/post.entity';
+import { PostModule } from './post/post.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'usuarios',
+      entities: [Usuario, Post],
+      synchronize: true,
+    }),
+    UsuarioModule,
+    PostModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
