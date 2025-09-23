f359328533160cc4ff295f6696e719fc4d35421a
luanapaesok <luanapaes.ok@gmail.com>
2025-09-23 08:24:06 -0300

    fear: add PostService

---
+++ src/post/post.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Post } from "./entity/post.entity";
+import { Repository } from "typeorm";
+import { CreatePostDTO } from "./dto/create-post.dto";
+import { UsuarioService } from "src/usuario/usuario.service";
+
+@Injectable()
+export class PostService {
+    constructor(
+        @InjectRepository(Post) private postRepository: Repository<Post>,
+        private usuarioService: UsuarioService
+    ) { }
+
+    getAll(){
+        return this.postRepository.find()
+    }
+
+    getPostByID(id: number){
+        return this.postRepository.findOne({
+            where: {
+                id
+            }, 
+            relations: ['user']
+        })
+    }
+
+    async create(data: CreatePostDTO) {
+        // let userID = await this.usuarioService.getByID(data.usuarioID!);
+        console.log(data)
+        let newPost: CreatePostDTO = {
+            usuarioID: data.usuarioID,
+            title: data.title,
+            description: data.description
+        }
+
+        let post = this.postRepository.create(newPost);
+        return await this.postRepository.save(post);
+    }
+
+    deleteAll(){
+        return this.postRepository.deleteAll();
+    }
+
+    deleteByID(id: number){
+        return this.postRepository.delete(id);
+    }
+}
