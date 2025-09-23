de5ad922d174a04cb68828d8d751abf0f6036125
luanapaesok <luanapaes.ok@gmail.com>
2025-09-23 08:22:07 -0300

    fear: add UsuarioEntity

---
+++ src/usuario/entity/usuario.entity.ts
@@ -0,0 +1,17 @@
+import { Post } from "src/post/entity/post.entity";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Usuario {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    nome: string;
+
+    @Column()
+    email: string;
+
+    @OneToMany(type => Post, post => post.usuarioID)
+    posts: Post[];
+}
