3d21a0a67b7a471c67c80cfa1989f0cab10c5071
luanapaesok <luanapaes.ok@gmail.com>
2025-09-23 09:54:53 -0300

    feat: add update funtion ao controller

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,34 +1,39 @@
-import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
 import { CreatePostDTO } from "./dto/create-post.dto";
 import { PostService } from "./post.service";
+import { UpdatePostDTO } from "./dto/update-post.dto";
 
 @Controller('post')
 export class PostController {
-    constructor(private postService: PostService){}
+    constructor(private postService: PostService) { }
 
     @Get('')
-    getAll(){
+    getAll() {
         return this.postService.getAll()
     }
 
     @Get(':id')
-    getByID(@Param('id') id: number){
+    getByID(@Param('id') id: number) {
         return this.postService.getPostByID(id);
     }
 
     @Post('')
-    create(@Body() data: CreatePostDTO){
+    create(@Body() data: CreatePostDTO) {
         return this.postService.create(data);
     }
 
     @Delete('')
-    deleteAll(){
+    deleteAll() {
         return this.postService.deleteAll();
     }
 
     @Delete(':id')
-    deleteByID(@Param('id') id: number){
+    deleteByID(@Param('id') id: number) {
         return this.postService.deleteByID(id);
     }
 
+    @Patch(':id')
+    update(@Body() updateUserDto: UpdatePostDTO) {
+        return this.postService.update(updateUserDto);
+    }
 }
