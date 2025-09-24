0b56b17a922dc43b24cee7e4cb29411f6ea9471f
luanapaesok <luanapaes.ok@gmail.com>
2025-09-24 10:43:58 -0300

    feat: add decorators às propriedades do DTO

--- src/usuario/dto/update-usuario.dto.ts
+++ src/usuario/dto/update-usuario.dto.ts
@@ -1,5 +1,12 @@
-export class UpdateUsuarioDTO{
+import { IsEmail, IsNumber, IsString } from "class-validator";
+
+export class UpdateUsuarioDTO {
+    @IsString()
     nome: string;
+
+    @IsEmail()
     email: string;
+
+    @IsNumber()
     id: number;
 }
