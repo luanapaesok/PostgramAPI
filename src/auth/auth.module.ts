efa40253d74780004a09e7005d767251779caac3
luanapaesok <luanapaes.ok@gmail.com>
2025-09-25 09:36:50 -0300

    feat: corrige importação do módulo User para o módulo Usuario

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,21 +2,20 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
-import { ConfigService } from '@nestjs/config';
-import { UsersModule } from 'src/users/users.module';
-import { UsersService } from 'src/users/users.service';
-import { User } from 'src/users/entity/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Usuario } from 'src/usuario/entity/usuario.entity';
+import { UsuarioService } from 'src/usuario/usuario.service';
+import { UsuarioModule } from 'src/usuario/usuario.module';
 
 @Module({
   imports: [
     JwtModule.register({
       secret: String(process.env.JWT_SECRET)
     }),
-    UsersModule, TypeOrmModule.forFeature([User])
+    UsuarioModule, TypeOrmModule.forFeature([Usuario])
   ],
   controllers: [AuthController],
-  providers: [AuthService, UsersService, User],
+  providers: [AuthService, Usuario, UsuarioService],
   exports: [AuthService]
 })
 export class AuthModule { }
