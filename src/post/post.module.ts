276e4ebb63973a3457cf26c9821e56dcfa623003
luanapaesok <luanapaes.ok@gmail.com>
2025-09-25 11:00:44 -0300

    feat: add AuthModule

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -4,12 +4,13 @@ import { PostService } from "./post.service";
 import { Post } from "./entity/post.entity";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { UsuarioModule } from "src/usuario/usuario.module";
+import { AuthModule } from "src/auth/auth.module";
 
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([Post]),
-        UsuarioModule
+        UsuarioModule, AuthModule
     ],
     controllers: [PostController],
     providers: [PostService, Post],
