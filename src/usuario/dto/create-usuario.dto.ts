bce801e27ba6441b5fa95edc401322984d8624dd
luanapaesok <luanapaes.ok@gmail.com>
2025-09-25 09:10:28 -0300

    feat: adiciona propriedade password

--- src/usuario/dto/create-usuario.dto.ts
+++ src/usuario/dto/create-usuario.dto.ts
@@ -1,9 +1,18 @@
-import { IsEmail, IsString } from "class-validator";
+import { IsEmail, IsString, IsStrongPassword } from "class-validator";
 
 export class CreateUsuarioDTO {
     @IsString()
     nome: string;
-    
+
     @IsEmail()
-    email: string
+    email: string;
+
+    @IsStrongPassword({
+        minLength: 6,
+        minLowercase: 0,
+        minUppercase: 0,
+        minNumbers: 0,
+        minSymbols: 0,
+    })
+    password: string;
 }
